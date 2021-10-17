7c759f5f67f1d2ebba871eeb8f8141437305eead
rammohanmiryala <rammohanmiryala@hotmail.com>
2021-10-17 20:10:08 +1030

    added uv index

--- script.js
+++ script.js
@@ -17,37 +17,95 @@ var searchEl = document.querySelector('#search-inpurt');
 var todaytempEl = document.querySelector('#today-temp')
 var todaywindEl = document.querySelector('#today-wind')
 var errormessgeEL = document.querySelector('.error-messge');
+var todayhumidityEl = document.querySelector('#today-humidity')
+var uvindexEl = document.querySelector('#uv-index')
+
+var lonvalue;
+var latvalue;
 
 
+var searchcity = function (event) {
+    event.preventDefault();
+
+    var cityname = searchEl.value.trim();
+
+    if (cityname) {
+        todayweather(cityname);
+        searchEl.value = '';
+
+    } else {
+        displaytime()
+    }
+};
 
 
-
-
-
-function getApi() {
-    if (searchEl.value == " "){
-        errormessgeEL.innerHTML = 'input is empty'
-       } 
-
+//funtion to get the current weather conditions for that city
+function todayweather() {
     var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + searchEl.value + '&appid=65d399c284fc843c554beb939eac0cb5';
     fetch(requestUrl)
-        .then(response => response.json())
-        .then(data => {
+        .then(function (response) {
+            return response.json();
+        })
+        //parmenters current wether api
+        .then(function (data) {
+            lonvalue = data['city']['coord']['lon'];
+            latvalue = data['city']['coord']['lat'];
+            uvvalue()
+            var datevalue = data['list'][0]['dt_txt']
             var namevalue = data['city']['name']
             var tempvalue = data['list'][0]['main']['temp']
             var windvalue = data['list'][0]['wind']['speed']
-            citynameEl.innerHTML = namevalue
-            todaytempEl.innerHTML = 'Temperature :' + tempvalue
-            todaywindEl.innerHTML = 'Wind speed  :' + windvalue
-
+            var todayhumidityvalue = data['list'][0]['main']['humidity']
+            citynameEl.innerHTML = namevalue + ' ' + ' ' + ' ( ' + datevalue.slice(0, 10) + ' ) '
+            var roundtemp = (((tempvalue - 273.15) * 9 / 5) + 32)
+            todaytempEl.innerHTML = (Math.round(roundtemp)) + " " + "F"
+            todayhumidityEl.innerHTML = todayhumidityvalue + " " + "%"
+            todaywindEl.innerHTML = windvalue + " " + "MPH"
+            todaywindEl.innerHTML = windvalue + " " + "MPH"
         })
         .catch(err => {
-            errormessgeEL.innerHTML = 'input is' + err
+            errormessgeEL.innerHTML = 'city name is not valid'
         })
 
+}
+
+
+function uvvalue() {
+    console.log(latvalue)
+    console.log(lonvalue)
+    var uvindexdata = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latvalue + '&lon=' + lonvalue + '&exclude=minutely,hourly&appid=65d399c284fc843c554beb939eac0cb5'
+    console.log(uvindexdata)
+    fetch(uvindexdata)
+        .then(function (response) {
+            return response.json();
+        })
+        //parmenters current wether api
+        .then(function (data) {
+            var indexvalue = data['current']['uvi']
+            uvindexEl.innerHTML = indexvalue
+        })
 
 }
-searchBtn.addEventListener('click', getApi)
+
+
+function displaytime() {
+    var time = 4;
+    var timerInterval = setInterval(function () {
+        time--;
+        errormessgeEL.textContent = "Please enter a cityname"
+        if (time === 0) {
+            clearInterval(timerInterval);
+            errormessgeEL.textContent = " "
+        }
+    }, 1000);
+
+}
+
+
+
+searchBtn.addEventListener('click', searchcity)
+
+searchBtn.addEventListener('click', uvvalue)
 
 
 
