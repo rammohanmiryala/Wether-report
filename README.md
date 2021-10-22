75d96dd93e3412dc1e4d8255236737763e778406
rammohanmiryala <rammohanmiryala@hotmail.com>
2021-10-22 21:04:44 +1030

    final commit with testing

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Wether-report-
+# Weather-report
+
+## About The Project
+
+This project is to get the current weather of a particular city. The first website is displayed with no date. when the user search the city, they get current weather and the following 5days weather as well, and the search is saved below the search button .
+
+## Technologies used
+
+- html
+- css
+- java script
+
+## Apis
+api.openweathermap.org/data/2.5/weather?q={city name}&appid=65d399c284fc843c554beb939eac0cb5
+https://api.openweathermap.org/data/2.5/onecall?{longitude}&{latitude}&exclude=hourly,daily&appid=5f6d35978b34af25b79ac449ccc5ee8c
+
+## functionality 
+- user need to click on the search button to get the current weather and fallowing 5 days 
+## Development process
+- HTML and CSS pages are created
+- added styling
+- added comments to HTML and CSS
+- collected APIs and tested it
+- written java script code 
+- final test on (22/10/2021)
+
+## The following appearance of the site:
+
+![my portfolio](./assets/wether.gif)
+
+# URL of the website
+
+ https://rammohanmiryala.github.io/Wether-report/
--- script.js
+++ script.js
@@ -10,42 +10,45 @@ var uvindexEl = document.querySelector('#uv-index')
 var datesEl = document.querySelector('.dates')
 var date1el = document.querySelector('#date')
 var bottomcardEl = document.querySelector('#bottomcard')
+var searchresultsEl = document.querySelector('.searchresults')
 
-
-
+// present date
 const todaytime = moment().format("YYYY-MM-DD")
-console.log(todaytime)
 
+// funtion to search citya and append the search//
 var searchcity = function (event) {
     event.preventDefault();
     var cityname = searchEl.value.trim();
+    // add the serach to the bottom of the div
     if (cityname) {
-        todayweather(cityname);
-
+        var addsearchreseultEl = document.createElement("button")
+        addsearchreseultEl.setAttribute("class", "searchcitybutton")
+        searchresultsEl.appendChild(addsearchreseultEl)
+        addsearchreseultEl.textContent = cityname
+        todayweather(cityname)
         searchEl.value = '';
-
     } else {
         displaytime()
     }
-
 };
-
+//  funtion that makes to dispay error messge for empty input
 function displaytime() {
     var time = 3;
     var timerInterval = setInterval(function () {
         time--;
-        errormessgeEL.textContent = "Please enter a cityname"
+        errormessgeEL.textContent = "'" + "Please enter a cityname" + "!" + "'"
         if (time === 0) {
             clearInterval(timerInterval);
             errormessgeEL.textContent = " "
         }
     }, 1000);
-
 }
+var lonvalue;
+var latvalue;
 
 //funtion to get the current weather conditions for that city
-function todayweather() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + searchEl.value + '&appid=65d399c284fc843c554beb939eac0cb5';
+function todayweather(cityname) {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityname + '&appid=65d399c284fc843c554beb939eac0cb5';
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
@@ -54,39 +57,42 @@ function todayweather() {
         .then(function (data) {
             lonvalue = data['city'].coord.lon;
             latvalue = data['city'].coord.lat;
-
             var datevalue = data['list'][0].dt_txt
             var namevalue = data['city'].name
             var tempvalue = data['list'][0].main.temp
             var windvalue = data['list'][0].wind.speed
             var todayhumidityvalue = data['list'][0].main.humidity
             citynameEl.innerHTML = namevalue + ' ' + ' ' + ' ( ' + datevalue.slice(0, 10) + ' ) '
-            var roundtemp = (((tempvalue - 273.15) * 9 / 5) + 32)
-            todaytempEl.innerHTML = (Math.round(roundtemp)) + " " + "F"
+            var roundtemp = (tempvalue - 273.15)
+            todaytempEl.innerHTML = (Math.round(roundtemp)) + " " + "℃"
             todaywindEl.innerHTML = windvalue + " " + "MPH"
             todayhumidityEl.innerHTML = todayhumidityvalue + " " + "%"
             uvvalue(lonvalue, latvalue)
 
         })
         .catch(err => {
-            var time = 3;
-            var timerInterval = setInterval(function () {
-                time--;
-                errormessgeEL.textContent = "city name is not valid"
-                if (time === 0) {
-                    clearInterval(timerInterval);
-                    errormessgeEL.textContent = " "
-                }
-            }, 1000);
+            notvalidate()
+
         })
 }
 
-var lonvalue;
-var latvalue;
 
+//  funtion that makes to dispay error messge for the invalid entry  
+function notvalidate() {
+
+    var time = 3;
+    var timerInterval = setInterval(function () {
+        time--;
+        errormessgeEL.textContent = "city name is not valid"
+        if (time === 0) {
+            clearInterval(timerInterval);
+            errormessgeEL.textContent = " "
+        }
+    }, 1000);
+}
+
+//  funtion that checks uvindex from onecall 
 function uvvalue() {
-    console.log(latvalue)
-    console.log(lonvalue)
     var uvindexdata = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latvalue + '&lon=' + lonvalue + '&exclude=minutely,hourly&appid=65d399c284fc843c554beb939eac0cb5'
     console.log(uvindexdata)
     fetch(uvindexdata)
@@ -96,26 +102,23 @@ function uvvalue() {
         .then(function (data) {
             var indexvalue = data['current']['uvi']
             uvindexEl.innerHTML = indexvalue
-            wetherfor5days()
+            wetherfor5days(latvalue, lonvalue)
 
         })
 }
+//  funtion that checks wether with lat and log 
+function wetherfor5days(latvalue, lonvalue) {
 
-
-function wetherfor5days() {
-
-    console.log(latvalue)
-    console.log(lonvalue)
     var next5days = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latvalue + '&lon=' + lonvalue + '&exclude=minutely,hourly&appid=65d399c284fc843c554beb939eac0cb5'
     console.log(next5days)
+
     fetch(next5days)
         .then(function (response) {
             return response.json();
         })
 
-        //parmenters current wether api
+        //parmenters current wether api that appendsChild
         .then(function (data) {
-            
             for (var i = 1; i < 6; i++) {
                 // create weatherday bottomdiv//
                 var weatherdayEl = document.createElement("div")
@@ -133,9 +136,6 @@ function wetherfor5days() {
                 weatherdayEl.appendChild(windEl)
                 weatherdayEl.appendChild(humidityEl)
 
-                
-
-                
                 var imagefor5days = data['daily'][i].weather[0].icon;
                 var iconurl = "http://openweathermap.org/img/w/" + imagefor5days + ".png";
 
@@ -144,143 +144,27 @@ function wetherfor5days() {
                 var windfor5days = data['daily'][i].wind_speed;
                 var humidityfor5days = data['daily'][i].humidity;
                 dayEl.textContent = moment(todaytime).add([i], 'd').format("YYYY-MM-DD");
-                var roundtemp5days = (((tempfor5days - 273.15) * 9 / 5) + 32)
+                var roundtemp5days = (tempfor5days - 273.15)
                 imgEl.textContent = iconurl
-                tempEl.textContent = 'Temp:' + " " + (Math.round(roundtemp5days)) + " " + "F"
+                tempEl.textContent = 'Temp:' + " " + (Math.round(roundtemp5days)) + " " + "℃"
                 windEl.textContent = 'wind:' + " " + windfor5days + " " + "MPH"
                 humidityEl.textContent = 'humidity:' + " " + humidityfor5days + " " + "%"
 
                 weatherdayEl.setAttribute("class", "weatherday")
                 dayEl.setAttribute("class", "dates")
                 imgEl.setAttribute("class", "climateimg")
-                imgEl.setAttribute("src", "iconurl")
+                imgEl.setAttribute("src", iconurl)
                 tempEl.setAttribute("class", "temps")
                 windEl.setAttribute("class", "winds")
                 humidityEl.setAttribute("class", "humidits")
 
+
+
             }
 
-        })
-    bottomcardEl.innerHTML = "";
+        });
+    bottomcardEl.innerHTML = ""
+
 
 }
-
 searchBtn.addEventListener('click', searchcity)
-
-
-
-/* 
-    
-    bottomcard
-    <div class="col-lg-2 weatherday">
-<p id="date1" class="dates">10/11/2021</p>
-<P id="temp1"class="temps">temp:</P>
-<img id="img1" src="" alt="img"class="climateimg"></img>
-<P id="wind1"class="winds">wind:</P>
-<P id="humidity1"class="humidits">humidity:</P>
-</div> */
-
-
-
-
-
-
-// function checkinput {
-//     var searchvalue = searchEl.value
-//     if (searchvalue == "") {
-//         errormessgeEL.innerHTML = 'input is empty'
-//     }
-//     if (!searchvalue) {
-//         errormessgeEL.innerHTML = '"No ciy is found"'
-//     }
-//     if (searchvalue==true) {
-//         return searchvalue
-
-//     }
-//     getApi()
-// }
-
-
-// function newFunction(data) {
-//     citynameEl.textContent = data['name'];
-//     console.log(data['name'])
-// }
-
-//searchBtn.addEventListener('click',getApi)        
-
-
-
-
-
-// function selectcityname() {
-//     var cityentered ="";
-//     var cityname =searchEl.value;
-//     console.log(cityname);
-
-//     // var requesturl = api.openweathermap.org/data/2.5/weather?q=cityname&appid=65d399c284fc843c554beb939eac0cb5;
-//     // $( function() {
-//     //     var availableTags = requesturl;
-//     //     $( "#tags" ).autocomplete({
-//     //       source: availableTags
-//     //     });
-//     // } );
-// }
-
-// function getapi(){
-//     var cityname =searchEl.value
-//     var requesturl = api.openweathermap.org/data/2.5/weather?q=cityname&appid=65d399c284fc843c554beb939eac0cb5;
-//     console(requesturl)
-
-// function wetherfor5days() {
-
-//     var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + searchEl.value + '&appid=65d399c284fc843c554beb939eac0cb5';
-//     fetch(requestUrl)
-
-//             //var roundtemp = (((tempvalue1 - 273.15) * 9 / 5) + 32)
-//             var datevalue1 = data['list'][6]['dt_txt']
-//             document.querySelector('#date1').innerHTML = ' ( ' + datevalue1.slice(0, 10) + ' ) '
-//             var datevalue2 = data['list'][14]['dt_txt']
-//             document.querySelector('#date2').innerHTML = ' ( ' + datevalue2.slice(0, 10) + ' ) '
-//             var datevalue3 = data['list'][22]['dt_txt']
-//             document.querySelector('#date3').innerHTML = ' ( ' + datevalue3.slice(0, 10) + ' ) '
-//             var datevalue4 = data['list'][30]['dt_txt']
-//             document.querySelector('#date4').innerHTML = ' ( ' + datevalue4.slice(0, 10) + ' ) '
-//             var datevalue5 = data['list'][38]['dt_txt']
-//             document.querySelector('#date5').innerHTML = ' ( ' + datevalue5.slice(0, 10) + ' ) '
-//             var tempvalue1 = data['list'][6]['main']['temp']
-//             document.querySelector('#temp1').innerHTML = (Math.round((tempvalue1 - 273.15) * 9 / 5) + 32) + " " + "F"
-//             var tempvalue2 = data['list'][14]['main']['temp']
-//             document.querySelector('#temp2').innerHTML = (Math.round((tempvalue2 - 273.15) * 9 / 5) + 32) + " " + "F"
-//             var tempvalue3 = data['list'][22]['main']['temp']
-//             document.querySelector('#temp3').innerHTML = (Math.round((tempvalue3 - 273.15) * 9 / 5) + 32) + " " + "F"
-//             var tempvalue4 = data['list'][30]['main']['temp']
-//             document.querySelector('#temp4').innerHTML = (Math.round((tempvalue4 - 273.15) * 9 / 5) + 32) + " " + "F"
-//             var tempvalue5 = data['list'][38]['main']['temp']
-//             document.querySelector('#temp5').innerHTML = (Math.round((tempvalue5 - 273.15) * 9 / 5) + 32) + " " + "F"
-//             var wind1 = data['list'][6]['wind']['speed']
-//             document.querySelector('#wind1').innerHTML = wind1 + " " + "MPH"
-//             var wind2 = data['list'][14]['wind']['speed']
-//             document.querySelector('#wind2').innerHTML = wind2 + " " + "MPH"
-//             var wind3 = data['list'][22]['wind']['speed']
-//             document.querySelector('#wind3').innerHTML = wind3 + " " + "MPH"
-//             var wind4 = data['list'][30]['wind']['speed']
-//             document.querySelector('#wind4').innerHTML = wind4 + " " + "MPH"
-//             var wind5 = data['list'][38]['wind']['speed']
-//             document.querySelector('#wind5').innerHTML = wind5 + " " + "MPH"
-//             var humidity1 = data['list'][6]['main']['humidity']
-//             document.querySelector('#humidity1').innerHTML = humidity1 + " " + "%"
-//             var humidity2 = data['list'][14]['main']['humidity']
-//             document.querySelector('#humidity2').innerHTML = humidity2 + " " + "%"
-//             var humidity3 = data['list'][22]['main']['humidity']
-//             document.querySelector('#humidity3').innerHTML = humidity3 + " " + "%"
-//             var humidity4 = data['list'][30]['main']['humidity']
-//             document.querySelector('#humidity4').innerHTML = humidity4 + " " + "%"
-//             var humidity5 = data['list'][38]['main']['humidity']
-//             document.querySelector('#humidity5').innerHTML = humidity + " " + "%"
-
-
-//         })
-
-
-
-// }
